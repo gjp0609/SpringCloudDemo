39864fa29fea70c98e68c8b31c9c281b50f2e2a7
JingPeng Guo <gjp0609@qq.com>
2018-06-07 15:24:45 +0800

    Eureka Producer

--- eureka/src/main/java/top/gjp0609/eureka/EurekaApplication.java
+++ eureka/src/main/java/top/gjp0609/eureka/EurekaApplication.java
@@ -1,5 +1,6 @@
 package top.gjp0609.eureka;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
---
+++ eureka_producer/src/main/java/top/gjp0609/eureka_client/EurekaProducerApplication.java
@@ -0,0 +1,14 @@
+package top.gjp0609.eureka_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class EurekaProducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaProducerApplication.class, args);
+    }
+}
